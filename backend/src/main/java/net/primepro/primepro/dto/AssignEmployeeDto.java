66592042e1d55af628bfd811972eff9155d3395e
binalitanya <binaliukwatte@gmail.com>
2024-09-22 15:18:13 +0530

    task/Task everything is completed

---
+++ backend/src/main/java/net/primepro/primepro/dto/AssignEmployeeDto.java
@@ -0,0 +1,13 @@
+package net.primepro.primepro.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class AssignEmployeeDto {
+    private Long taskId;
+    private Long employeeId;
+}
